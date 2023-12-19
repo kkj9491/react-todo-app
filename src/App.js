54bc3c7d78e7e3c0bd5b0a7ecd7a27d1f949d7e5
김광진 <kj.kim@ttb.kr>
2023-12-20 08:21:03 +0900

    tailwindcss 적용

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React, { useState } from "react";
 import "./App.css"
+import List from "./components/List";
+import Form from "./components/Form";
 
 
 export default function App() {
 
-
     const [todoData, setTodoData] = useState([]);
     const [value, setValue] = useState("");
-
-
-    // todoData = [
-    //     {
-    //         id: "1",
-    //         title: "공부하기",
-    //         completed: true
-    //     },
-    //     {
-    //         id: "2",
-    //         title: "청소하기",
-    //         completed: false
-    //     }
-    // ]
-
-    const handleClick = (id) =>{
-        let newTodoData = todoData.filter((data) => data.id !== id);
-        setTodoData(newTodoData)
-    }
-
-    const handleChange = (e) => {
-        setValue(e.target.value)
-    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -46,67 +24,15 @@ export default function App() {
         setValue("");
     }
 
-    const btnStyle = {
-        color: "#fff",
-        border: "none",
-        padding: "5px 9px",
-        borderRadius: "50%",
-        cursor: "pointer",
-        float: "right"
-    }
-
-    const getStyle = (completed) => {
-        return {
-            padding: "10px",
-            borderBottom: "1px #ccc dotted",
-            textDecoration: completed ? "line-through" : "none",
-        }
-    }
-
-    const handleCompleteChange = (id) => {
-        let newTodoData = todoData.map(data => {
-            if(data.id === id) {
-                data.completed = !data.completed;
-            }
-            return data;
-        })
-
-        setTodoData(newTodoData)
-    }
-
-
     return (
-        <div className="container">
-            <div className="todoBlock">
-                <div className="title">
+        <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+            <div className="w-full p-6 m-4 bg-white rounded shadow lg:max-w-lg">
+                <div className="flex justify-between mb-3">
                     <h1>할일 목록</h1>
                 </div>
+                <List todoData={todoData} setTodoData={setTodoData}  />
 
-                {todoData.map((data) => (
-                    <div style={getStyle(data.completed)} key={data.id}>
-                        <input type="checkbox" defaultChecked={false} onChange={() => handleCompleteChange(data.id)} />
-                        {data.title}
-                        <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
-                    </div>
-                ))}
-
-
-                <form style={{display: 'flex'}} onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        name="value"
-                        style={{flex: '10', padding: '5px'}}
-                        placeholder="해야 할 일을 입력하세요."
-                        value={value}
-                        onChange={handleChange}
-                    />
-                    <input
-                        type="submit"
-                        value="입력"
-                        className="btn"
-                        style={{flex: '1'}}
-                    />
-                </form>
+                <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
             </div>
 
         </div>
